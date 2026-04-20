a5d43caa0706980207ecf82787d83c29db45ccf0
Akhilesh Sharma <akhileshsharma.ldh2004@gmail.com>
2026-04-20 15:09:53 +0530

    added button component

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -5,7 +5,7 @@ function Button({ children, onClick, type = "button" }) {
       onClick={onClick}
       className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition"
     >
-      {children}
+      {children}click
     </button>
   );
 }
