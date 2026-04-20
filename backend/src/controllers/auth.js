fdac7cdc72db16dc43297190c863575dfa4e7d7c
Mukesh <mukeshyaduvanshi9026@gmail.com>
2026-04-20 16:12:50 +0530

    create auth logic

---
+++ backend/src/controllers/auth.js
@@ -0,0 +1,124 @@
+import bcrypt from "bcrypt";
+import { User } from "./models/User.js";
+import { signupSchema } from "../validators/auth.js";
+import user from "../models/user.js";
+
+export const signup = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const parsedBody = signupSchema.safeParse(body);
+
+    if (!parsedBody.success) {
+      return res.status(400).json({
+        success: false,
+        msg: "Invalid data",
+        errors: parsedBody.error.issues,
+      });
+    }
+
+    const { fullName, email, password, country } = parsedBody.data;
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({
+        success: false,
+        msg: "User already exists",
+      });
+    }
+
+   
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    
+    const newUser = new User({
+      fullName,
+      email,
+      password: passwordHash,
+      country,
+    });
+
+    await newUser.save();
+
+    return res.status(201).json({
+      success: true,
+      msg: "User registered successfully",
+    });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      msg: "User can't be registered. Please try again",
+    });
+  }
+};
+
+//login
+export const login = async (req, res) => {
+  try {
+    const body = req.body;
+
+    
+    const parsedBody = loginSchema.safeParse(body);
+
+
+    if (!parsedBody.success) {
+      return res.status(400).json({
+        success: false,
+        msg: "Invalid credentials",
+        errors: parsedBody.error.errors.map((e) => ({
+          field: e.path[0],
+          message: e.message,
+        })),
+      });
+    }
+
+    const { email, password } = parsedBody.data;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "User does not exist. Please signup first.",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        msg: "Incorrect password",
+      });
+    }   
+    const payload = {
+      id: user._id,
+      email: user.email,
+      name: user.name,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+ res.cookie("token", token, { expires: new Date(Date.now() + 24 * 3600000) });
+
+    return res.status(200).json({
+      success: true,
+      msg: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token, 
+    });
+
+  } catch (err) {
+    
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      msg: "Failed to login. Please try again.",
+    });
+  }
+};
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,16 +3,28 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: true,
+    required: true,
     trim: true,
   },
   email: {
     type: String,
     required: true,
+    unique:true,
+    lowercase:true,
     trim: true,
   },
   password: {
     type: String,
     required: true,
+    unique:true
   },
+  country:{
+    type:String,
+    required:true
+  }
+},
+{
+  timestamp:true
 });
+
+module.exports = mongoose.model("User",userSchema)
